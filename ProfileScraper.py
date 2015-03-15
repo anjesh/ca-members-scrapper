632b465de9240c3b078ac2558ea44226607a5c46
Anjesh Tuladhar <anjeshtuladhar@gmail.com>
2015-03-15 09:32:29 +0545

    added more scraper and tests

--- ProfileScraper.py
+++ ProfileScraper.py
@@ -6,19 +6,23 @@ import re
 class ProfileFields:
     NAME = 100
     DOB = 101
-    BIRTH_DISTRICT = 200
-    BIRTH_VDC = 201
-    BIRTH_WARD = 202
+
+    BIRTH_HEADER = 200
+    BIRTH_DISTRICT = 201
+    BIRTH_VDC = 202
+    BIRTH_WARD = 203
     
-    PADDRESS_DISTRICT = 300
-    PADDRESS_VDC = 301
-    PADDRESS_WARD = 302
-    PADDRESS_TOLE = 303
+    PADDRESS_HEADER = 300
+    PADDRESS_DISTRICT = 301
+    PADDRESS_VDC = 302
+    PADDRESS_WARD = 303
+    PADDRESS_TOLE = 304
     
-    KADDRESS_DISTRICT = 300
-    KADDRESS_VDC = 301
-    KADDRESS_WARD = 302
-    KADDRESS_TOLE = 303
+    KADDRESS_HEADER = 320
+    KADDRESS_DISTRICT = 321
+    KADDRESS_VDC = 322
+    KADDRESS_WARD = 323
+    KADDRESS_TOLE = 324
 
     CONTACT_MOBILE = 400
     CONTACT_EMAIL = 401
@@ -51,22 +55,23 @@ class ProfileFields:
     PUBLICATIONS = 130
     FOREIGN_VISITS = 140
 
-class FieldPatterns:
-    def setPatterns(self):
-        self.patterns[ProfileFields.NAME] = '<A name=[0-9]*></a>([^<]*)<';
-        self.patterns[ProfileFields.DOB] = 'hGd ldlt M </span><span[^>]*>([^<]*)<';
-
 class Profile: 
     def __init__(self):
         self.values = {}
         pass
 
+    def __str__(self):
+        s = ""
+        for f in self.values:
+            s = s + "\r\nField " + str(f) + ": " + self.values[f]
+        return s
+
     def setFieldValue(self, field, value):
         self.values[field] = value
 
-class ProfileScraper:
-    def __init__(self, profile):
-        self.profile = profile
+class LinePatternFinder:
+    def __init__(self):
+        self.stage = 0;
         pass
 
     def setLine(self, line):
@@ -84,24 +89,21 @@ class ProfileScraper:
 
     def process(self):
         self.checkName() or \
-        self.checkDob() or self.checkBirthPlace() or\
-        self.checkVDCMunicipality() or self.checkWard()
+        self.checkDob() or \
+        self.checkBirthDistrict() or self.checkBirthVDC() or self.checkBirthWard() or \
+        self.checkPAddressHeader() or self.checkPAddressDistrict() or self.checkPAddressVDC() or self.checkPAddressWard() or self.checkPAddressTole() or \
+        self.checkKAddressHeader() or self.checkKAddressDistrict() or self.checkKAddressVDC() or self.checkKAddressWard() or self.checkKAddressTole() or \
+        self.checkEducationQualifcation() or self.checkEducationMajor() or \
+        self.checkParty() or self.checkPartyStartedYear() or \
+        1
             
-    def checkOneLineFieldPattern(self, fieldname, pattern):
-        m = re.findall(pattern, self.line)
-        if m and m[0]:
-            self.foundField = fieldname
-            self.value = m[0]
-            self.profile.setFieldValue(self.foundField, self.value)
-            return True
-
     def checkName(self):
         # <A name=1></a>df=  cOGb| ;'Gb/ g]DjfË<br>
         m = re.findall('<A name=[0-9]*></a>([^<]*)<', self.line)
-        if m and m[0]:            
+        if m and m[0]:
+            self.stage = 0;            
             self.foundField = ProfileFields.NAME
             self.value = m[0]
-            self.profile.setFieldValue(self.foundField, self.value)
             return True
         return False
 
@@ -111,39 +113,162 @@ class ProfileScraper:
         if m and m[0]:
             self.foundField = ProfileFields.DOB
             self.value = m[0]
-            self.profile.setFieldValue(self.foundField, self.value)
             return True
         return False
 
-    def checkBirthPlace(self):
+    def checkBirthDistrict(self):
         # hGd :yfg M lhNnf M kfFry/<br>
         m = re.findall('hGd :yfg M ([^<]*)<', self.line)
         if m and m[0]:
+            self.stage = ProfileFields.BIRTH_HEADER
             self.foundField = ProfileFields.BIRTH_DISTRICT
-            self.value = m[0]
-            self.profile.setFieldValue(self.foundField, self.value)
+            self.value = m[0]            
             return True
         return False
 
-    def checkVDCMunicipality(self):
+    def checkBirthVDC(self):
         # uf=lj=;=÷g=kf= M ODj'ª<br>
         m = re.findall('uf=lj=;=÷g=kf= M ([^<]*)<', self.line)
-        if m and m[0]:
+        if self.stage == ProfileFields.BIRTH_HEADER and m and m[0]:
             self.foundField = ProfileFields.BIRTH_VDC
             self.value = m[0]
-            self.profile.setFieldValue(self.foundField, self.value)
             return True
         return False
 
-    def checkWard(self):
+    def checkBirthWard(self):
         # j8f g+= M !<br>
         m = re.findall('j8f g\+= M ([^<]*)<', self.line)
-        if m and m[0]:
+        if self.stage == ProfileFields.BIRTH_HEADER and m and m[0]:
             self.foundField = ProfileFields.BIRTH_WARD
             self.value = m[0]
-            self.profile.setFieldValue(self.foundField, self.value)
             return True
         return False
+
+    def checkPAddressHeader(self):
+        m = re.findall(':yfoL 7]ufgf M', self.line)
+        if m and m[0]:
+            self.stage = ProfileFields.PADDRESS_HEADER
+            self.foundField = ProfileFields.PADDRESS_HEADER
+            self.value = ""
+            return True
+        return False
+
+    def checkPAddressDistrict(self):
+        #lhNnf M kfFry/<br>
+        m = re.findall('lhNnf M ([^<]*)<', self.line)
+        if self.stage == ProfileFields.PADDRESS_HEADER and m and m[0]:
+            self.foundField = ProfileFields.PADDRESS_DISTRICT
+            self.value = m[0]
+            return True
+        return False
+
+    def checkPAddressVDC(self):
+        #lhNnf M kfFry/<br>
+        m = re.findall('uf=lj=;=÷g=kf= M ([^<]*)<', self.line)
+        if self.stage == ProfileFields.PADDRESS_HEADER and m and m[0]:
+            self.foundField = ProfileFields.PADDRESS_VDC
+            self.value = m[0]
+            return True
+        return False
+
+    def checkPAddressWard(self):
+        #lhNnf M kfFry/<br>
+        m = re.findall('j8f g\+= M ([^<]*)<', self.line)
+        if self.stage == ProfileFields.PADDRESS_HEADER and m and m[0]:
+            self.foundField = ProfileFields.PADDRESS_WARD
+            self.value = m[0]
+            return True
+        return False
+
+    def checkPAddressTole(self):
+        #6f]n M em08]gu/<br>
+        m = re.findall('6f]n M ([^<]*)<', self.line)
+        if self.stage == ProfileFields.PADDRESS_HEADER and m and m[0]:
+            self.foundField = ProfileFields.PADDRESS_TOLE
+            self.value = m[0]
+            return True
+        return False
+
+    def checkKAddressHeader(self):
+        m = re.findall('sf7df08f} pkTosfsf] 7]ufgf M<br>', self.line)
+        if m and m[0]:
+            self.stage = ProfileFields.KADDRESS_HEADER
+            self.foundField = ProfileFields.KADDRESS_HEADER
+            self.value = ""
+            return True
+        return False
+
+    def checkKAddressDistrict(self):
+        #lhNnf M kfFry/<br>
+        m = re.findall('lhNnf M ([^<]*)<', self.line)
+        if self.stage == ProfileFields.KADDRESS_HEADER and m and m[0]:
+            self.foundField = ProfileFields.KADDRESS_DISTRICT
+            self.value = m[0]
+            return True
+        return False
+
+    def checkKAddressVDC(self):
+        m = re.findall('uf=lj=;=÷g=kf= M ([^<]*)<', self.line)
+        if self.stage == ProfileFields.KADDRESS_HEADER and m and m[0]:
+            self.foundField = ProfileFields.KADDRESS_VDC
+            self.value = m[0]
+            return True
+        return False
+
+    def checkKAddressWard(self):
+        m = re.findall('j8f g\+= M ([^<]*)<', self.line)
+        if self.stage == ProfileFields.KADDRESS_HEADER and m and m[0]:
+            self.foundField = ProfileFields.KADDRESS_WARD
+            self.value = m[0]
+            return True
+        return False
+
+    def checkKAddressTole(self):
+        m = re.findall('6f\]n M ([^<]*)<', self.line)
+        if self.stage == ProfileFields.KADDRESS_HEADER and m and m[0]:
+            self.foundField = ProfileFields.KADDRESS_TOLE
+            self.value = m[0]
+            return True
+        return False
+
+    def checkEducationQualifcation(self):
+        #z}lIfs of]Uotf M :gfts<br>
+        m = re.findall('z\}lIfs of\]Uotf M ([^<]*)<', self.line)
+        if m and m[0]:
+            self.foundField = ProfileFields.EDUCATION_QUALIFICATION
+            self.value = m[0]
+            return True
+        return False
+
+    def checkEducationMajor(self):
+        #cWoogsf] ljifo M dfgljsL<br>
+        m = re.findall('cWoogsf\] ljifo M ([^<]*)<', self.line)
+        if m and m[0]:
+            self.foundField = ProfileFields.EDUCATION_MAJOR
+            self.value = m[0]
+            return True
+        return False
+
+    def checkParty(self):
+        #bnLo ;+nUgtf M t/fO{­dw]z nf]stflGqs kf6L{<br>
+        m = re.findall('bnLo ;\+nUgtf M ([^<]*)<', self.line)
+        if m and m[0]:
+            self.foundField = ProfileFields.PARTY
+            self.value = m[0]
+            return True
+        return False
+
+    def checkPartyStartedYear(self):
+        #/fhgLlts bndf cfa4 ePsf] jif{ M lj=;= @)#^<br>
+        m = re.findall('/fhgLlts bndf cfa4 ePsf\] jif\{ M ([^<]*)<', self.line)
+        if m and m[0]:
+            self.foundField = ProfileFields.PARTY_STARTED_YEAR
+            self.value = m[0]
+            return True
+        return False
+
+
+
 
 
 
--- test.py
+++ test.py
@@ -10,7 +10,7 @@ from ProfileScraper import *
 class ScaperTest(unittest.TestCase):
     def setUp(self):
         self.profile = Profile();
-        self.scraper =  ProfileScraper(self.profile)
+        self.scraper =  LinePatternFinder()
 
     def testName(self):
         self.scraper.setLine("<A name=1></a>df=  cOGb| ;'Gb/ g]DjfË<br>")
@@ -22,20 +22,102 @@ class ScaperTest(unittest.TestCase):
         self.assertEqual(self.scraper.getFoundField(), ProfileFields.DOB);
         self.assertEqual(self.scraper.getFieldValue(), "2034/07/12");
 
-    def testBirthPlace(self):
+    def testBirthDistrict(self):
         self.scraper.setLine("hGd :yfg M lhNnf M kfFry/<br>")
         self.assertEqual(self.scraper.getFoundField(), ProfileFields.BIRTH_DISTRICT);
         self.assertEqual(self.scraper.getFieldValue(), "lhNnf M kfFry/");
 
-    def testVDCMunicipality(self):
+    def testBirthVDC(self):
+        self.scraper.setLine("hGd :yfg M lhNnf M kfFry/<br>")
         self.scraper.setLine("uf=lj=;=÷g=kf= M ODj'ª<br>")
         self.assertEqual(self.scraper.getFoundField(), ProfileFields.BIRTH_VDC);
         self.assertEqual(self.scraper.getFieldValue(), "ODj'ª");
 
-    def testWard(self):
+    def testBirthWard(self):
+        self.scraper.setLine("hGd :yfg M lhNnf M kfFry/<br>")
         self.scraper.setLine("j8f g+= M !<br>")
         self.assertEqual(self.scraper.getFoundField(), ProfileFields.BIRTH_WARD);
         self.assertEqual(self.scraper.getFieldValue(), "!");
+
+    def testPAddressHeader(self):
+        self.scraper.setLine(":yfoL 7]ufgf M")
+        self.assertEqual(self.scraper.getFoundField(), ProfileFields.PADDRESS_HEADER);
+        self.assertEqual(self.scraper.getFieldValue(), "");
+
+    def testPAddressDistrict(self):
+        self.scraper.setLine(":yfoL 7]ufgf M")
+        self.scraper.setLine("lhNnf M kfFry/<br>")
+        self.assertEqual(self.scraper.getFoundField(), ProfileFields.PADDRESS_DISTRICT);
+        self.assertEqual(self.scraper.getFieldValue(), "kfFry/");
+
+    def testPAddressVDC(self):
+        self.scraper.setLine(":yfoL 7]ufgf M")
+        self.scraper.setLine("uf=lj=;=÷g=kf= M s[i0fgu/<br>")
+        self.assertEqual(self.scraper.getFoundField(), ProfileFields.PADDRESS_VDC);
+        self.assertEqual(self.scraper.getFieldValue(), "s[i0fgu/");
+
+    def testPAddressWard(self):
+        self.scraper.setLine(":yfoL 7]ufgf M")
+        self.scraper.setLine("j8f g+= M &<br>")
+        self.assertEqual(self.scraper.getFoundField(), ProfileFields.PADDRESS_WARD);
+        self.assertEqual(self.scraper.getFieldValue(), "&");
+
+    def testPAddressTole(self):
+        self.scraper.setLine(":yfoL 7]ufgf M")
+        self.scraper.setLine("6f]n M em08]gu/<br>")
+        self.assertEqual(self.scraper.getFoundField(), ProfileFields.PADDRESS_TOLE);
+        self.assertEqual(self.scraper.getFieldValue(), "em08]gu/");
+
+    def testKAddressHeader(self):
+        self.scraper.setLine("sf7df08f} pkTosfsf] 7]ufgf M<br>")
+        self.assertEqual(self.scraper.getFoundField(), ProfileFields.KADDRESS_HEADER);
+        self.assertEqual(self.scraper.getFieldValue(), "");
+
+    def testKAddressDistrict(self):
+        self.scraper.setLine("sf7df08f} pkTosfsf] 7]ufgf M<br>")
+        self.scraper.setLine("lhNnf M kfFry/<br>")
+        self.assertEqual(self.scraper.getFoundField(), ProfileFields.KADDRESS_DISTRICT);
+        self.assertEqual(self.scraper.getFieldValue(), "kfFry/");
+
+    def testKAddressVDC(self):
+        self.scraper.setLine("sf7df08f} pkTosfsf] 7]ufgf M<br>")
+        self.scraper.setLine("uf=lj=;=÷g=kf= M s[i0fgu/<br>")
+        self.assertEqual(self.scraper.getFoundField(), ProfileFields.KADDRESS_VDC);
+        self.assertEqual(self.scraper.getFieldValue(), "s[i0fgu/");
+
+    def testKAddressWard(self):
+        self.scraper.setLine("sf7df08f} pkTosfsf] 7]ufgf M<br>")
+        self.scraper.setLine("j8f g+= M &<br>")
+        self.assertEqual(self.scraper.getFoundField(), ProfileFields.KADDRESS_WARD);
+        self.assertEqual(self.scraper.getFieldValue(), "&");
+
+    def testKAddressTole(self):
+        self.scraper.setLine("sf7df08f} pkTosfsf] 7]ufgf M<br>")
+        self.scraper.setLine("6f]n M em08]gu/<br>")
+        self.assertEqual(self.scraper.getFoundField(), ProfileFields.KADDRESS_TOLE);
+        self.assertEqual(self.scraper.getFieldValue(), "em08]gu/");
+
+    def testEducationQualification(self):
+        self.scraper.setLine("z}lIfs of]Uotf M :gfts<br>")
+        self.assertEqual(self.scraper.getFoundField(), ProfileFields.EDUCATION_QUALIFICATION);
+        self.assertEqual(self.scraper.getFieldValue(), ":gfts");
+
+    def testEducationMajor(self):
+        self.scraper.setLine("cWoogsf] ljifo M dfgljsL<br>")
+        self.assertEqual(self.scraper.getFoundField(), ProfileFields.EDUCATION_MAJOR);
+        self.assertEqual(self.scraper.getFieldValue(), "dfgljsL");
+
+    def testParty(self):
+        self.scraper.setLine("bnLo ;+nUgtf M t/fO{­dw]z nf]stflGqs kf6L{<br>")
+        self.assertEqual(self.scraper.getFoundField(), ProfileFields.PARTY);
+        self.assertEqual(self.scraper.getFieldValue(), "t/fO{­dw]z nf]stflGqs kf6L{");
+
+
+    def testPartyStartedYear(self):
+        self.scraper.setLine("/fhgLlts bndf cfa4 ePsf] jif{ M lj=;= @)#^<br>")
+        self.assertEqual(self.scraper.getFoundField(), ProfileFields.PARTY_STARTED_YEAR);
+        self.assertEqual(self.scraper.getFieldValue(), "lj=;= @)#^");
+
 
 unittest.main()
 
